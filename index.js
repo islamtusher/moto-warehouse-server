83004e80dff856a9178acaf822706c255786dd36
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-05-06 18:51:08 +0600

    connect to db and use gitignore

--- index.js
+++ index.js
@@ -9,9 +9,26 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-    res.send('Moto WareHouse server running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.txqt9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const servicesCollection = client.db('stocks').collection('moto-collection')
+        console.log('conncet to db');
+
+        // server home
+        app.get('/', async (req, res) => {
+            res.send('Moto WareHouse server running')
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
+
 
 app.listen(port, () => {
     console.log('Listing Port:', port);
