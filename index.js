9d7fdaedddcfb6d7fab852356bc998555dfdf3e5
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-05-10 21:27:42 +0600

    change cors polyce code

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const port = process.env.PORT || 5000
 const app = express();
 require('dotenv').config();
 
-app.use(cors())
+// enable cors
+app.use(
+    cors({
+      origin: true,
+      optionsSuccessStatus: 200,
+      credentials: true,
+    })
+  );
+  app.options(
+    '*',
+    cors({
+      origin: true,
+      optionsSuccessStatus: 200,
+      credentials: true,
+    })
+  );
+
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.txqt9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
